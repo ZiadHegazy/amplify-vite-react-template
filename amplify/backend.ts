516d8b436558bf59c6f84a8abe9ecda7cebf5915
ZiadHegazy99 <ziad.hegazy@student.guc.edu.eg>
2024-08-06 13:56:23 +0300

    lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-
+import { function1 } from './functions/function1/resource';
 defineBackend({
   auth,
   data,
+  function1,
 });
